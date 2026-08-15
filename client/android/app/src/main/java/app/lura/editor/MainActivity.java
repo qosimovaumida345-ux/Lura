cf93a52ab02da441c3609d819a786dfb485320f0
Umida Qosimova <qosimovaumida345@gmail.com>
2026-08-16 02:57:09 +0500

    Initialize Capacitor and Tauri properly and fix build.yml

---
+++ client/android/app/src/main/java/app/lura/editor/MainActivity.java
@@ -0,0 +1,5 @@
+package app.lura.editor;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lura.editor',
+  appName: 'Lura',
+  webDir: 'dist'
+};
+
+export default config;
